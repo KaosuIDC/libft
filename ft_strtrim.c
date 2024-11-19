540d797bd3761d04d094008b261f5203299327ee
Sullivan Delory <sudelory@z2r4p4.42lyon.fr>
2024-11-19 19:01:57 +0100

    Nov 19 2024 19:01

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,33 +6,47 @@
 /*   By: sudelory <sudelory@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 13:34:19 by sudelory          #+#    #+#             */
-/*   Updated: 2024/11/19 17:09:14 by sudelory         ###   ########.fr       */
+/*   Updated: 2024/11/19 19:01:20 by sudelory         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include  "libft.h"
-int	begin_str(const char *s1, char const *to_trim)
+#include "libft.h"
+
+int	*str_begin(char *s1, char *to_trim)
 {
-	int	i;
-	int	j;
+	size_t	i;
+	size_t	j;
+	int		trim;
 
 	i = 0;
-	j = 0;
-	while(s1[i])
+	trim = 0;
+	while (s1[i])
 	{
-		ft_strchr;
-		if(s1[i] == to_trim[j])
+		j = 0;
+		while (to_trim[j])
+		{
+			if (s1[i] == to_trim[j])
+			{
+				trim++;
+				i++;
+				j -= 1;
+			}
+			j++;
+		}
+		if (to_trim[j] == '\0')
+			return (trim);
 	}
+	return (trim);
 }
 
-int	end_str(const char *s1, char const *to_trim)
+int	*str_end(char *s1, char *to_trim)
 {
-	int	i;
-	int	j;
-	ft_strrchr;
+	size_t	i;
+	size_t	j;
+	int		trim;
 }
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	
+
 }
