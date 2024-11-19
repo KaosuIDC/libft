bd1f78a6181e4aac9624faf39cb995af9c770e99
Sullivan Delory <sudelory@z1r12p5.42lyon.fr>
2024-11-19 17:09:35 +0100

    Nov 19 2024 17:09

---
+++ ft_strtrim.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sudelory <sudelory@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/19 13:34:19 by sudelory          #+#    #+#             */
+/*   Updated: 2024/11/19 17:09:14 by sudelory         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include  "libft.h"
+int	begin_str(const char *s1, char const *to_trim)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while(s1[i])
+	{
+		ft_strchr;
+		if(s1[i] == to_trim[j])
+	}
+}
+
+int	end_str(const char *s1, char const *to_trim)
+{
+	int	i;
+	int	j;
+	ft_strrchr;
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	
+}
